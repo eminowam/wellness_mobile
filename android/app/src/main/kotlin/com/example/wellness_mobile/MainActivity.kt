c4aaeecf48ce3e0fbf4b3cdc09edd591cf4e673d
aimeerimeminova <aimemynoo@gmail.com>
2023-12-19 12:01:45 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/wellness_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wellness_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:wellness_mobile/app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
